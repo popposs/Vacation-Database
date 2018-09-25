ff783df2a3c68d7936fdc0d39df6df669ce7c3df
Josh Chang <jchang82@illinois.edu>
2018-09-24 20:59:57 -0500

    poop

--- app.py
+++ app.py
@@ -18,24 +18,20 @@ app.secret_key = os.environ['APP_SECRET_KEY']
 def signup():
     form = SignupForm()
     if form.validate_on_submit():
-        try:
-            arrival =  datetime.strptime(form.arrival_date.data.strftime('%x'), "%m/%d/%y").date()
-            departure = datetime.strptime(form.departure_date.data.strftime('%x'), "%m/%d/%y").date()
+        arrival =  datetime.strptime(form.arrival_date.data.strftime('%x'), "%m/%d/%y").date()
+        departure = datetime.strptime(form.departure_date.data.strftime('%x'), "%m/%d/%y").date()
 
-            signup = Reservation(name=form.name.data, email=form.email.data, arrival_date=arrival, departure_date=departure)
-            db_session.add(signup)
-            db_session.commit()
+        signup = Reservation(name=form.name.data, email=form.email.data, arrival_date=arrival, departure_date=departure)
+        db_session.add(signup)
+        db_session.commit()
 
-            availability = get_availability(arrival, departure)
-            print('\n>>>\n', arrival , departure, availability, '\n>>>\n')
+        availability = get_availability(arrival, departure)
+        print('\n>>>\n', arrival , departure, availability, '\n>>>\n')
 
-            arrival_str = datetime.strptime(arrival, '%m/%d/%y')
-            departure_str = datetime.strptime(departure, '%m/%d/%y')
+        arrival_str = datetime.strftime(arrival, '%m/%d/%y')
+        departure_str = datetime.strftime(departure, '%m/%d/%y')
 
-            return "Unique Registration ID: {} for {} to {}".format(db_session.query(Reservation.id).filter_by(name=form.name.data).filter_by(email=form.email.data).first()[0], arrival_str, departure_str)
-        except Exception as e:
-            print(e)
-            return "{}".format(e)
+        return "Unique Registration ID: {} for {} to {}".format(db_session.query(Reservation.id).filter_by(name=form.name.data).filter_by(email=form.email.data).first()[0], arrival_str, departure_str)
     return render_template('signup.html', form=form)
 
 @app.route("/reserved", methods=['GET'])
